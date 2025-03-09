af8b942c4780da0d9fa2ab31b34d9f106dedbe00
Yan <yans@yancomm.net>
2025-03-09 15:09:11 -0700

    fix paths

--- crosstool/build-sample.sh
+++ crosstool/build-sample.sh
@@ -2,7 +2,7 @@
 
 sample=$1
 
-[ -e ../x-tools/$sample ] && echo "Already built: $sample" && exit
+[ -e ../toolchains/$sample ] && echo "Already built: $sample" && exit
 HOME=$(dirname $PWD) CT_PREFIX=$(dirname $PWD)/toolchains ./ct-ng $sample
 yes '' | HOME=$(dirname $PWD) CT_PREFIX=$(dirname $PWD)/toolchains ./ct-ng build.$(nproc)
 rm -rf .build/$sample
